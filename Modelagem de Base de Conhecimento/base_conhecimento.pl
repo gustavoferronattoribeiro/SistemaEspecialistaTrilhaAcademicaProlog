95d31b1d0a377bad8869f5644532f7c7d50b8d58
Gustavo Ferronatto Ribeiro <145728708+gustavoferronattoribeiro@users.noreply.github.com>
2025-09-26 18:59:28 -0300

    Add files via upload

---
+++ Modelagem de Base de Conhecimento/base_conhecimento.pl
@@ -0,0 +1,50 @@
+% base_conhecimento.pl
+
+% fatos: trilha(Nome, Descrição)
+trilha(ciencia_de_dados, 'Análise e interpretação de grandes volumes de dados.').
+trilha(deep_learning, 'Criação de redes neurais para resolver problemas complexos.').
+trilha(desenvolvimento_web, 'Construção e manutenção de sites e aplicativos web.').
+trilha(inteligencia_artificial, 'Criação de sistemas que podem aprender e tomar decisões.').
+trilha(redes_e_infraestrutura, 'Gerenciamento de redes de computadores e infraestrutura de TI.').
+trilha(seguranca_da_informacao, 'Proteção de sistemas, redes e dados contra ameaças.').
+trilha(visao_computacional, 'Desenvolvimento de sistemas que "veem" e interpretam imagens.').
+
+% fatos: perfil(Trilha, Característica, Peso)
+perfil(ciencia_de_dados, matematica_estatistica, 5).
+perfil(ciencia_de_dados, analise_critica, 5).
+perfil(ciencia_de_dados, curiosidade, 4).
+
+perfil(deep_learning, matematica_estatistica, 5).
+perfil(deep_learning, logica_computacional, 5).
+perfil(deep_learning, pesquisa_cientifica, 4).
+
+perfil(desenvolvimento_web, logica_computacional, 4).
+perfil(desenvolvimento_web, design_visual, 3).
+perfil(desenvolvimento_web, trabalho_em_equipe, 4).
+
+perfil(inteligencia_artificial, matematica_estatistica, 5).
+perfil(inteligencia_artificial, logica_computacional, 4).
+perfil(inteligencia_artificial, resolucao_de_problemas, 4).
+
+perfil(redes_e_infraestrutura, resolucao_de_problemas, 4).
+perfil(redes_e_infraestrutura, atencao_a_detalhes, 4).
+perfil(redes_e_infraestrutura, trabalho_em_equipe, 3).
+
+perfil(seguranca_da_informacao, resolucao_de_problemas, 5).
+perfil(seguranca_da_informacao, curiosidade, 4).
+perfil(seguranca_da_informacao, atencao_a_detalhes, 5).
+
+perfil(visao_computacional, matematica_estatistica, 4).
+perfil(visao_computacional, logica_computacional, 5).
+perfil(visao_computacional, resolucao_de_problemas, 4).
+
+% fatos: pergunta(ID, Texto, Característica)
+pergunta(1, 'Você se interessa por como as coisas funcionam?', curiosidade).
+pergunta(2, 'Você tem afinidade com matemática e estatística?', matematica_estatistica).
+pergunta(3, 'Você gosta de resolver quebra-cabeças e problemas complexos?', resolucao_de_problemas).
+pergunta(4, 'Você tem um olhar apurado para detalhes?', atencao_a_detalhes).
+pergunta(5, 'Você gosta de trabalhar em equipe?', trabalho_em_equipe).
+pergunta(6, 'Você tem um bom senso estético ou interesse em design?', design_visual).
+pergunta(7, 'Você se interessa por lógica e raciocínio abstrato?', logica_computacional).
+pergunta(8, 'Você gosta de ler artigos científicos e fazer pesquisas?', pesquisa_cientifica).
+pergunta(9, 'Você é bom em analisar informações e tirar conclusões?', analise_critica).
